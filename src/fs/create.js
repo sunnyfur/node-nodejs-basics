b1bd66fdf2017e2372f9d990469deb6a307d9011
sunnyfur <polin.makarova@mail.ru>
2022-12-03 11:52:14 +0500

    feat: create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,18 @@
+import { access, constants, writeFile } from "fs";
+
 const create = async () => {
-    // Write your code here 
+  const file = new URL("files/fresh.txt", import.meta.url);
+
+  access(file, constants.F_OK, (err) => {
+    if (err) {
+      // create file and write
+      writeFile(file, "I am fresh and young", (err) => {
+        if (err) console.log(err);
+      });
+    } else {
+      throw new Error("FS operation failed");
+    }
+  });
 };
 
 await create();
