9b9ea999cf1de39966e0e94145e18841af98072f
Polina <sunnyfur9@gmail.com>
2022-12-04 21:24:18 +0500

    feat: solve copy fs

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,21 @@
+import { Console } from "console";
+import { access, constants, copyFile, mkdir } from "fs";
+import { readdir } from "fs/promises";
+import { dirname, join } from "path";
+
 const copy = async () => {
-    // Write your code here 
+  const folder = new URL("files/", import.meta.url);
+  const destination = new URL("files_copy/", import.meta.url);
+
+  await mkdir(destination, { recursive: false }, (err) => {
+    if (err) throw new Error("FS operation failed");
+  });
+  const files = await readdir(folder);
+  for (const file of files) {
+    await copyFile(new URL(file, folder), new URL(file, destination), (err) => {
+      if (err) console.log(err);
+    });
+  }
 };
 
 copy();
